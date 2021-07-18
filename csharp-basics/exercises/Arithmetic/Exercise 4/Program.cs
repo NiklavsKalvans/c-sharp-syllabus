0daa641ef26ce367d2b8e3fe535872f0d8d56a34
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-07-18 11:28:26 +0300

    Exercise 4 complete.

---
+++ csharp-basics/exercises/Arithmetic/Exercise 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Product1ToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int product = 1;
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                product *= arr[i];
+            }
+            Console.WriteLine(product);
+        }
+    }
+}
