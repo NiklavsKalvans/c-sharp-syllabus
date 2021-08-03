a807ba34c91a09e97512553b67fc8b88170f5574
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-03 17:37:06 +0300

    Exercise 1 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Product
+    {
+        string name;
+        double priceAtStart;
+        int amountAtStart;
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            this.name = name;
+            this.priceAtStart = priceAtStart;
+            this.amountAtStart = amountAtStart;
+        }
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{name}, price {priceAtStart}, amount {amountAtStart}");
+        }
+        public void ChangePrice(double newPrice)
+        {
+            this.priceAtStart = newPrice;
+        }
+        public void ChangeAmount(int newAmount)
+        {
+            this.amountAtStart = newAmount;
+        }
+    }
+}
