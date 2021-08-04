2afe0b99e520e268af448c2be7d01ed4cec191ef
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-04 15:01:52 +0300

    Exercise 2 complete.

--- csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Car.cs
+++ csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Car.cs
@@ -2,32 +2,39 @@
 {
     public class Car
     {
+        double startKilometers;
+        double endKilometers;
+        double liters;
+
         public Car(double startOdo)
-        {          
+        {
+            this.startKilometers = startOdo;
         }
 
         public double CalculateConsumption()
         {
-            return 0;
+            return (endKilometers - startKilometers) / liters;
         }
 
         private double ConsumptionPer100Km()
         {
-            return 0;
+            return (liters / (endKilometers - startKilometers)) * 100;
         }
 
         public bool GasHog()
         {
-           return ConsumptionPer100Km() > 15
+            return ConsumptionPer100Km() > 15;
         }
 
         public bool EconomyCar()
         {
-            return ConsumptionPer100Km() < 5
+            return ConsumptionPer100Km() < 5;
         }
 
         public void FillUp(int mileage, double liters)
         {
+            this.endKilometers = mileage;
+            this.liters = liters;
         }
     }
 }
