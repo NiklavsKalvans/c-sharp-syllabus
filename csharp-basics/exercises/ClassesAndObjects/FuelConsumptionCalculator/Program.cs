778588b7a724c5cfb842db1a7316a44c7650fd8b
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-04 15:48:23 +0300

    Exercise FuelConsumptionCalculator complete.

--- csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Program.cs
@@ -14,20 +14,22 @@ namespace FuelConsumptionCalculator
             int liters;
 
             Car car = new Car(0);
-            Car car1 = new Car(1200);
+            Car car1 = new Car(0);
 
-            Console.Write("Enter first reading: ");
-            startKilometers = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter liters reading: ");
-            liters = Convert.ToInt32(Console.ReadLine());
-            car.FillUp(startKilometers, liters);
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter first reading: ");
+                startKilometers = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter liters reading: ");
+                liters = Convert.ToInt32(Console.ReadLine());
+                car.FillUp(startKilometers, liters);
 
-            Console.Write("Enter first reading: ");
-            startKilometers = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter liters reading: ");
-            liters = Convert.ToInt32(Console.ReadLine());
-            car1.FillUp(startKilometers, liters);
-
+                Console.Write("Enter first reading: ");
+                startKilometers = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter liters reading: ");
+                liters = Convert.ToInt32(Console.ReadLine());
+                car1.FillUp(startKilometers, liters);
+            }
             Console.WriteLine("Car kilometers per liter are " + car.CalculateConsumption() + " gasHog:" + car.GasHog());
             Console.WriteLine("Car1 Kilometers per liter are " + car1.CalculateConsumption() + " economyCar:" + car1.EconomyCar());
             Console.ReadKey();
