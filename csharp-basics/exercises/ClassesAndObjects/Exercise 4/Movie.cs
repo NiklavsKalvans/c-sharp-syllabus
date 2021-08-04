d803e91a59d123c60c3dd08feda212b50530686e
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-04 18:53:39 +0300

    Exercise 4 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class Movie
+    {
+        public string _title;
+        public string _studio;
+        public string _rating;
+
+        public Movie (string title, string studio, string rating)
+        {
+            this._title = title;
+            this._studio = studio;
+            this._rating = rating;
+        }
+
+        public Movie (string title, string studio)
+        {
+            this._title = title;
+            this._studio = studio;
+            this._rating = "PG";
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie[] movies = new Movie[3];
+            movies[0] = new Movie("Casino Royale", "Eon Productions", "PG-13");
+            movies[1] = new Movie("Glass", "Buena Vista International", "PG-13");
+            movies[2] = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine($"{movies[i]._title} {movies[i]._studio} {movies[i]._rating}");
+            }
+
+            Movie[] moviesPG = new Movie[movies.Length];
+            moviesPG = GetPG(movies);
+
+            for (int i = 0; i < moviesPG.Length; i++)
+            {
+                Console.WriteLine($"{moviesPG[i]._title} {moviesPG[i]._studio} {moviesPG[i]._rating}");
+            }
+        }
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            return movies.Where(movie => movie._rating == "PG").ToArray();
+        }
+    }
+}
