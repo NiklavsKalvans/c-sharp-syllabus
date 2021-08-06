27f39ada0e225a44ca63091ac482e7915b158127
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-06 11:13:38 +0300

    Exercise 7 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Dog
+    {
+        public string _name;
+        public string _sex;
+        public string _father;
+        public string _mother;
+        public Dog(string name, string sex)
+        {
+            _name = name;
+            _sex = sex;
+            _father = "Unknown";
+            _mother = "Unknown";
+        }
+
+        public static void FathersName(Dog dog)
+        {
+            Console.WriteLine(dog._father);
+        }
+
+        public static bool HasSameMotherAs(Dog dog1, Dog dog2)
+        {
+            return (dog1._mother == dog2._mother);
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class DogTest
+    {
+        static void Main(string[] args)
+        {
+            string sex1 = "male";
+            string sex2 = "female";
+            string[] names = { "Max", "Rocky", "Sparky", "Buster", "Sam", "Lady", "Molly", "Coco" };
+            Dog dog1 = new Dog(names[0], sex1);
+            Dog dog2 = new Dog(names[1], sex1);
+            Dog dog3 = new Dog(names[2], sex1);
+            Dog dog4 = new Dog(names[3], sex1);
+            Dog dog5 = new Dog(names[4], sex1);
+            Dog dog6 = new Dog(names[5], sex2);
+            Dog dog7 = new Dog(names[6], sex2);
+            Dog dog8 = new Dog(names[7], sex2);
+
+            dog1._father = dog2._name;
+            dog1._mother = dog6._name;
+            dog8._father = dog4._name;
+            dog8._mother = dog7._name;
+            dog2._father = dog5._name;
+            dog2._mother = dog7._name;
+            dog4._father = dog3._name;
+            dog4._mother = dog6._name;
+
+            Dog.FathersName(dog8);
+            Dog.FathersName(dog3);
+            Console.WriteLine(Dog.HasSameMotherAs(dog8, dog2));
+        }
+    }
+}
