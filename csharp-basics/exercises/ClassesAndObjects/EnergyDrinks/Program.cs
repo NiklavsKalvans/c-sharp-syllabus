b3ec6ecf804bf10bd6ce138008c1a240d772c4a5
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-06 10:23:21 +0300

    EnergyDrinks complete.

--- csharp-basics/exercises/ClassesAndObjects/EnergyDrinks/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/EnergyDrinks/Program.cs
@@ -14,22 +14,20 @@ namespace EnergyDrinks
 
         private static void Main(string[] args)
         {
-            //fixme
-            /*
              Console.WriteLine("Total number of people surveyed " + NumberedSurveyed);
-             Console.WriteLine("Approximately " + energyDrinkers + " bought at least one energy drink");
-             Console.WriteLine(preferCitrus + " of those " + "prefer citrus flavored energy drinks.");
-             */
+             Console.WriteLine("Approximately " + CalculateEnergyDrinkers(NumberedSurveyed) + " bought at least one energy drink");
+             Console.WriteLine(CalculatePreferCitrus(NumberedSurveyed) + " of those " + "prefer citrus flavored energy drinks.");
+             Console.ReadKey();
         }
 
-        double CalculateEnergyDrinkers(int numberSurveyed)
+        static double CalculateEnergyDrinkers(int numberSurveyed)
         {
-            throw new Exception("Sorry, no code written :(");
+            return NumberedSurveyed * PurchasedEnergyDrinks;
         }
 
-        double CalculatePreferCitrus(int numberSurveyed)
+        static double CalculatePreferCitrus(int numberSurveyed)
         {
-            throw new Exception("Sorry, no code written :(");
+            return NumberedSurveyed * PurchasedEnergyDrinks * PreferCitrusDrinks;
         }
     }
 }
