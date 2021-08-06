4421045f1ebe3d99187d7aabaf6fe2db6861d410
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-06 15:02:32 +0300

    Exercise 9 complete.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(2, 7);
+            Point point2 = new Point(4, 9);
+            Console.WriteLine($"Coordinates of the 1st point {point1._x} {point1._y}");
+            Console.WriteLine($"Coordinates of the 2nd point {point2._x} {point2._y}");
+            SwapPoints(point1, point2);
+            Console.WriteLine("Coordinates ater swap:");
+            Console.WriteLine($"Coordinates of the 1st point {point1._x} {point1._y}");
+            Console.WriteLine($"Coordinates of the 2nd point {point2._x} {point2._y}");
+        }
+
+        public static void SwapPoints(Point a, Point b)
+        {
+            int placeHolderX = a._x;
+            int placeHolderY = a._y;
+            a._x = b._x;
+            a._y = b._y;
+            b._x = placeHolderX;
+            b._y = placeHolderY;
+        }
+    }
+}
