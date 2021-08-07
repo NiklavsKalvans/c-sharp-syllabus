b9f305807f442fdd6aed79079c2cde35ebca0f68
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-07 08:43:02 +0300

    BankAccount complete!

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        public string _name;
+        public double _balance;
+
+        public BankAccount (string name, double balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -10,6 +10,19 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            BankAccount acc1 = new BankAccount("Rob", 250.5);
+            string report = ShowUserNameAndBalance(acc1);
+            Console.WriteLine(report);
+            Console.ReadLine();
+        }
+
+        public static string ShowUserNameAndBalance(BankAccount acc)
+        {
+            if (acc._balance >= 0)
+                return ($"{acc._name}, $") + string.Format("{0:F2}", acc._balance);
+            else
+                acc._balance *= -1;
+                return ($"{acc._name}, -$") + string.Format("{0:F2}", acc._balance);
         }
     }
 }
