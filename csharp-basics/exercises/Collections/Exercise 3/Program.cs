420b320cfbf2ccc41e0ec8aa3ab781fde8e5d328
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-08 12:18:15 +0300

    Exercise 3 complete.

---
+++ csharp-basics/exercises/Collections/Exercise 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the names you want to add to a list");
+            var names = new List<string>();
+            bool isInput = true;
+            while (isInput)
+            {
+                var inputs = Console.ReadLine();
+                if (!string.IsNullOrEmpty(inputs))
+                    names.Add(inputs);
+                else isInput = false;
+            }
+            Console.WriteLine("Unique names in your list:");
+            List<string> namesUnique = names.Distinct().ToList();
+            Console.WriteLine(string.Join(",", namesUnique));
+            Console.ReadKey();
+        }
+    }
+}
