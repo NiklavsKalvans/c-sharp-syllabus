6b2d2a78a938e1201aa0cbcdba3e6c59e50adf42
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-19 11:53:30 +0300

    Exercise AdApp complete.

---
+++ csharp-basics/exercises/Polymorphism/AdApp/Poster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdApp
+{
+    class Poster : Advert
+    {
+        private int _height;
+        private int _width;
+        private int _copies;
+        private int _rate;
+
+        public Poster(int fee, int height, int width, int copies, int rate) : base(fee)
+        {
+            _height = height;
+            _width = width;
+            _copies = copies;
+            _rate = rate;
+        }
+
+        public new int Cost()
+        {
+            var fee = base.Cost() + (_copies * _rate);
+            return fee;
+        }
+
+        public override string ToString()
+        {
+            return $"\nPoster: Cost = {Cost()}";
+        }
+    }
+}
