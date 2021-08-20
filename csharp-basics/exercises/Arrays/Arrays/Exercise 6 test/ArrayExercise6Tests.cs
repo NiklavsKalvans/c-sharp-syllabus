4c0cb38fc2e794986eaa611be66358a5eccf7f1a
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-20 11:46:03 +0300

    restart

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 6 test/ArrayExercise6Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercise_6_test
+{
+    [TestClass]
+    public class ArrayExercise6Tests
+    {
+        [TestMethod]
+        public void Array_ArrayRandomNumbers_CopiedAndReplacedLast()
+        {
+            int[] arr = new int[] { 29, 25, 72, 48, 59, 88, 3, 55, 46, 33 };
+            int[] arr2 = new int[arr.Length];
+            int num = -7;
+            CopyArrayAndReplaceLast(arr, arr2, num);
+        }
+    }
+}
---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 6 test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Exercise 6 test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Exercise 6 test")]
+[assembly: AssemblyCopyright("Copyright © HP 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1741a430-ab51-4724-8243-b7e8d8d46b0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
