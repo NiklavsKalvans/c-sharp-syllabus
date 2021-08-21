858b3922697db4027b3b776cf78ff697e1b34223
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-21 18:56:39 +0300

    Arithmetics7.Tests complete.

---
+++ csharp-basics/exercises/Arithmetic/Arithmetics7.Tests/Arithmetics7Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GravityCalculator;
+
+namespace Arithmetics7.Tests
+{
+    [TestClass]
+    public class Arithmetics7Tests
+    {
+        [TestMethod]
+        public void CalculatePosition_Time10s_Minus490point5()
+        {
+            //Arrange
+            double time = 10.0;
+            double expected = -490.5;
+            double result;
+
+            //Act
+            result = Program.CalculatePosition(time);
+
+            //Assert
+            Assert.AreEqual(Math.Round(expected, 1), Math.Round(result, 1));
+        }
+    }
+}
--- csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -3,17 +3,22 @@ using System;
 
 namespace GravityCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            double gravity = -9.81;  // Earth's gravity in m/s^2
-            double initialVelocity = 0.0;
             double fallingTime = 10.0;
-            double initialPosition = 0.0;
-            double finalPosition = 0.0;
+            double finalPosition = CalculatePosition(fallingTime);
             Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
             Console.ReadKey();
+        }
+
+        public static double CalculatePosition(double time)
+        {
+            double gravity = -9.81;
+            double initialVelocity = 0.0;
+            double initialPosition = 0.0;
+            return 0.5 * gravity * time * time + initialVelocity * time + initialPosition;
         }
     }
 }
