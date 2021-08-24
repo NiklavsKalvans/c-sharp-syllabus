4359ef468df3f9e56b0d8d01f2b1a9ab323f3f0b
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-08-24 09:14:44 +0300

    Polymorphism pull request changes!

--- csharp-basics/exercises/Polymorphism/Hierarchy/Program.cs
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Program.cs
@@ -5,14 +5,14 @@ namespace Hierarchy
 {
     class Program
     {
+        static List<Animal> zoo = new List<Animal>();
         static void Main(string[] args)
         {
-            var zoo = new List<Animal>();
             Console.WriteLine("Lets make some animals and feed them!");
             var input = Console.ReadLine();
             while (input != "End")
             {
-                var animal = AddAnimal(input, zoo);
+                var animal = ProcessAnimal(input);
                 input = Console.ReadLine();
                 var inputSplit = input.Split(" ");
                 if (inputSplit[0] == "Vegetable")
@@ -34,34 +34,34 @@ namespace Hierarchy
                 Console.Write($"{animal}, ");
             }
         }
-        public static Animal AddAnimal(string input, List<Animal> list)
+        public static Animal ProcessAnimal(string input)
         {
             var inputSplit = input.Split(" ");
             if (inputSplit[0] == "Cat")
             {
                 var cat = new Cat(inputSplit[0], inputSplit[1], Convert.ToDouble(inputSplit[2]), inputSplit[3], inputSplit[4]);
-                list.Add(cat);
+                zoo.Add(cat);
                 cat.MakeSound();
                 return cat;
             }
             else if (inputSplit[0] == "Tiger")
             {
                 var tiger = new Tiger(inputSplit[0], inputSplit[1], Convert.ToDouble(inputSplit[2]), inputSplit[3]);
-                list.Add(tiger);
+                zoo.Add(tiger);
                 tiger.MakeSound();
                 return tiger;
             }
             else if (inputSplit[0] == "Zebra")
             {
                 var zebra = new Zebra(inputSplit[0], inputSplit[1], Convert.ToDouble(inputSplit[2]), inputSplit[3]);
-                list.Add(zebra);
+                zoo.Add(zebra);
                 zebra.MakeSound();
                 return zebra;
             }
             else if (inputSplit[0] == "Mouse")
             {
                 var mouse = new Mouse(inputSplit[0], inputSplit[1], Convert.ToDouble(inputSplit[2]), inputSplit[3]);
-                list.Add(mouse);
+                zoo.Add(mouse);
                 mouse.MakeSound();
                 return mouse;
             }
